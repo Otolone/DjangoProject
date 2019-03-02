c4ebe2f292773254e5a2fa021310f9f45a64fea4
mypc <nyenty@yahoo.com>
2019-03-02 07:40:03 +0100

    Added to capture data and implement button functions

--- todolist/views.py
+++ todolist/views.py
@@ -1,11 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import TodoList
 
+from .forms import TodoListForm
 # Create your views here. index
+
+from django.views.decorators.http import require_POST
 
 def index(request):
     todo_items = TodoList.objects.order_by('id') 
-    context = {'todo_items':todo_items}
+    form = TodoListForm()
+    context = {'todo_items':todo_items, 'form':form}
     return render(request,'todolist/index.html',context)
 
-    
+
+@require_POST
+def addTodoItem(request):
+    form = TodoListForm(request.POST)
+    if form.is_valid():
+        new_todo = TodoList(text=request.POST['text'])
+        new_todo.save()
+
+    return redirect('index') 
+
+
+def completedTodo(request,todo_id):
+    todo = TodoList.objects.get(pk=todo_id)
+    todo.completed = True
+    todo.save()
+    return redirect('index')    
+
+def deleteCompleted(request):
+    TodoList.objects.filter(completed__exact=True).delete()
+    return redirect('index')
+
+
+def deleteAll(request):
+    TodoList.objects.all().delete()
+    return redirect('index')
+
